ec105f4245e7e563ba06dc66a8584cc7f588a250
x3x3x3 <live9367@outlook.com>
2021-09-07 05:35:57 +0800

    fun_Promise

---
+++ Promise_get.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+function getFile(fpath){
+    return new Promise(function(resolve,reject){
+        fs.readFile(fpath,'utf8',(err,dataStr)=>{
+            if(err) return reject(err);
+            resolve(dataStr);
+        });
+    })
+}
+
+getFile('./a.js').then((r1)=>{console.log(r1)},(err)=>{console.log(err)});
+
