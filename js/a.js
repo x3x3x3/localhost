985befe8fca21f8a7648980cfbde64a9b5afb7b2
x3x3x3 <live9367@outlook.com>
2021-09-07 02:47:43 +0800

    first commit

---
+++ js/a.js
@@ -0,0 +1,7 @@
+let n1 = 10;
+
+function show(){
+    return n1;
+}
+
+console.log(n1);
---
+++ node/t.js
@@ -0,0 +1,12 @@
+let n1 = 10;
+let n2 = 20;
+
+
+function show(){
+    console.log(n1+n2);
+}
+
+
+export default{
+    n1,n2,show
+}
