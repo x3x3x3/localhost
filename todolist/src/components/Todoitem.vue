6702cbed10d2a2159f0c57537b00dc63177e5ec1
x3x3x3 <live9367@outlook.com>
2021-09-12 02:33:31 +0800

    'vue_components'

---
+++ todolist/src/components/Todoitem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>
+    {{index}}---{{item}}
+    <button @click="remove(index)">X</button>
+  </li>
+</template>
+
+<script>
+export default {
+ props:['item','index'],
+ methods:{
+   remove(index){
+     this.$emit('delete',this.index);
+   }
+ }
+}
+</script>
+
+<style scoped>
+
+</style>
